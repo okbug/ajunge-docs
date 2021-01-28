bc2a5446e212c24c3bda5115e8dec5a02f1321ca
okbug <1583978087@qq.com>
2021-01-28 23:28:25 +0800

    1

--- kkb25/03.react/1.context/src/App.js
+++ kkb25/03.react/1.context/src/App.js
@@ -1,5 +1,21 @@
 import React from 'react'
-
+let element = (
+  <div id="mainApp">
+    <h1 className={(function(){return 'hhh'})()}>哈哈哈</h1>
+  </div>
+)
+console.log(JSON.stringify(element, null, 2))
+class Test {
+  age;
+  constructor(name) {
+    this.name = name
+  }
+}
+let test = new Test('haha')
+console.log(test)
+for(let  i in test) {
+  console.log(i, test.hasOwnProperty(i))
+}
 export default class App extends React.Component {
   render() {
     return (<>
