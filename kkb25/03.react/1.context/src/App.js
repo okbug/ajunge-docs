f70d1f956c057d028c7ece90e794f3e231324199
okbug <1583978087@qq.com>
2021-01-29 20:46:38 +0800

    !

--- kkb25/03.react/1.context/src/App.js
+++ kkb25/03.react/1.context/src/App.js
@@ -1,25 +1,20 @@
-import React from 'react'
-let element = (
-  <div id="mainApp">
-    <h1 className={(function(){return 'hhh'})()}>哈哈哈</h1>
-  </div>
-)
-console.log(JSON.stringify(element, null, 2))
-class Test {
-  age;
-  constructor(name) {
-    this.name = name
-  }
-}
-let test = new Test('haha')
-console.log(test)
-for(let  i in test) {
-  console.log(i, test.hasOwnProperty(i))
+import React, { useEffect, useState } from 'react'
+
+function A() {
+  const [am, b] = useState(0)
+  useEffect(() => {
+    console.log(1)
+  })
+  return (<>
+    <h2 onClick={b(am + 1)}>{am}</h2>
+    h3
+  </>)
 }
 export default class App extends React.Component {
   render() {
     return (<>
       <h1>App</h1>
+      <A></A>
     </>)
   }
 }
