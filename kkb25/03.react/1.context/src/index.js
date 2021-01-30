36723a134311e5f3b2bd5f46b79fb1bac969a3aa
okbug <1583978087@qq.com>
2021-01-30 09:46:24 +0800

    react-dom

--- kkb25/03.react/1.context/src/index.js
+++ kkb25/03.react/1.context/src/index.js
@@ -13,7 +13,26 @@ ReactDOM.render(
 
 
 
-import React from './my/react'
+// let element = React.createElement('h1', {id:'haha',className:'baba',style:{color:'red',background:'green'}}, 'world','11')
 
-let element = React.createElement('h1', {id:'haha'}, 'world','11')
-console.log(element);
+/* 
+自己实现的render函数支持JSX
+此注释内所有内容都可以运行
+import React from './my/react'
+import ReactDOM from './my/react-dom'
+let element = <h1 className="haha" style={{color:'red',background:'green'}}>Hello World</h1>
+ReactDOM.render(element, document.getElementById('root'))
+*/
+// let element = <h1 className="haha" style={{color:'red',background:'green'}}>Hello World</h1>
+import React from './my/react'
+import ReactDOM from './my/react-dom'
+function App(props) {
+  return (<div style={{color:'red',background:'green'}}>
+    <span>你好:{props.name}</span>
+    <span>{props.children}</span>
+  </div>)
+  // return <h1 className="haha" style={{color:'red',background:'green'}}>Hello World</h1>
+}
+let element = <App name="阿君哥">哈哈哈哈哈</App>
+console.log(element)
+ReactDOM.render(element, document.getElementById('root'))
--- kkb25/03.react/1.context/src/my/react-dom.js
+++ kkb25/03.react/1.context/src/my/react-dom.js
@@ -1,10 +1,84 @@
-const ReactDOM = {}
+const ReactDOM = {
+  render
+}
+
 /**
  * 
  * @param {*} vdom 需要渲染的虚拟节点 
  * @param {*} container 在public/index.html 中的真实dom 一般为 document.getElementByID('root')
  */
 function render(vdom, container) {
+  let dom = createDOM(vdom) // dom 是真实的dom
+  container.appendChild(dom)
+}
 
+/**
+ * 
+ * @param {Object} vdom 虚拟dom
+ * @return {HTMLElement} dom 真实dom
+ */
+function createDOM(vdom) {
+  if (typeof vdom === 'string' || typeof vdom === 'number') { // <h1>我只是一段文字</h1>
+    return document.createTextNode(vdom)
+  }
+  let { type, props } = vdom // type:'h1',props:{}
+  let dom
+  if (typeof type === 'function') { // 如果是函数组件
+    return updateFunctionComponent(vdom)
+  } else { // 如果是普通字符串
+    dom = document.createElement(type) // 创建真实DOM元素
+  }
+  updateProps(dom, props) // 给这个DOM元素加上attributes
+  // 处理ChildNodes
+  if (typeof props.children === 'string' || typeof props.children === 'number') {
+    dom.textContent = props.children // textContent | innerText | innerHTML
+  } else if (typeof props.children === 'object' && props.children.type) {
+    render(props.children, dom)
+  } else if (Array.isArray(props.children)) {// children 是数组
+    reconcileChildren(props.children, dom)
+  } else {
+    dom.textContent = props.children ? props.children.toString() : ''
+  }
+  // 现在这个dom已经完整了
+  return dom
+
+}
+
+function updateFunctionComponent(vdom) {
+  let { type, props } = vdom
+  // type是函数组件的函数，函数中传入props吧，所以我们直接执行这个函数，并且他的返回值就是一个React虚拟DOM
+  let renderedVDOM = type(props) // renderedVDOM 就是CreateElement的返回对象，此时给这个虚拟DOM再转为真实节点就OK
+  return createDOM(renderedVDOM)
+}
+
+/**
+ * 
+ * @param {*} children DOM元素中的子节点
+ * @param {*} dom 真实DOM HTMLElement
+ */
+function reconcileChildren(children, dom) {
+  for (let i = 0; i < children.length; i++) {
+    render(children[i], dom) // 把每一个children都变成真实dom，并且添加在根容器中
+  }
+}
+
+/**
+ * 更新除了children以外其他的attributes
+ * @param {*} dom  真实的DOM元素
+ * @param {*} props attributes or 标签中的内容
+ */
+function updateProps(dom, props) {
+  for (let key in props) {
+    if (key === 'children') {
+      continue
+    } else if (key === 'style') { // 处理是style的情况
+      let style = props[key]
+      for (let attr in style) {
+        dom.style[attr] = style[attr] // dom.style['color'] = 'red' 这是DOM的固有方法，所以要这样做
+      }
+    } else {
+      dom[key] = props[key]
+    }
+  }
 }
 export default ReactDOM
