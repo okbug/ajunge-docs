6c2a5e7ce28954403b7fb47b4e12ad86c9a540e2
okbug <1583978087@qq.com>
2021-02-17 20:17:17 +0800

    fsdfsdf

---
+++ kkb25/02.vue/Test.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1 @click="handleClick">TESTVUE</h1>
+    <ul>
+      <li v-for="item in list" :key="item">{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'name',
+    data() {
+      return {
+        list:['A','B','C','D']
+      }
+    },
+    methods: {
+      handleClick() {
+        this.list = ['D','C','B','A']
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
