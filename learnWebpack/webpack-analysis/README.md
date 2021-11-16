fe03fc547445b6affcce24e09de547a8a5d795ad
okbug <1583978087@qq.com>
2021-11-17 07:59:21 +0800

    docs: webpack 懒加载

--- learnWebpack/webpack-analysis/README.md
+++ learnWebpack/webpack-analysis/README.md
@@ -1,3 +1,5 @@
 /1.sync => webpack同步加载commonjs
 
 /2.cjs2esm => commonjs加载esmodule
+
+/3.esm2esm => esm 加载esm
--- learnWebpack/webpack-analysis/src/index.js
+++ learnWebpack/webpack-analysis/src/index.js
@@ -1,3 +1,11 @@
-const title = require('./title.js')
+import title, { age } from "./title";
 
-console.log(title, title.age)
+console.log(title, age);
+export const a = 1;
+
+const btn = document.getElementById('btn')
+btn.onclick = () => {
+    import('./lazy.js').then(e => {
+        console.log(e);
+    })
+}
