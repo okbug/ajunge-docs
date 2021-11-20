945c7d9a5f92a5959442aa585baaad0e574e981f
okbug <1583978087@qq.com>
2021-11-20 08:53:18 +0800

    feat: 对class转换插件优化

--- learnWebpack/ast-demo/3.js
+++ learnWebpack/ast-demo/3.js
@@ -11,7 +11,14 @@ class A {
     }
 
     getName() {
+        console.log(name)
         return this.name
+    }
+
+    setName(newName) {
+        console.log(newName);
+        if (newName === this.name) return;
+        this.name = name;
     }
 }
 `
@@ -23,18 +30,18 @@ let res2 = core.transform(code2, {
 const myClassTransformPlugin = {
     visitor: {
         ClassDeclaration(nodePath) {
-            // console.log(nodePath)
             let {node} = nodePath;
             let {id} = node;
             let body = []
-            let classMethods = node.body.body; // 方法： constructor, getName
+            let classMethods = node.body.body; // 方法： constructor, getName等等
             classMethods.forEach(method => {
                 if (method.kind === 'construcotr') { // 构造函数的话，就创建一个函数
                     let ctorFunc = types.functionDeclaration(id, method.params, method.body, method.generator, method.async);
                     body.push(ctorFunc);
                 } else { // 类上的方法
+                    const key = method.key.name === 'constructor' ? id : method.key;
                     let left = types.memberExpression(types.memberExpression(id, types.identifier('prototype')), method.key)
-                    let right = types.functionExpression(method.key /* 函数的名字 这里传null就是匿名函数 */, method.params, method.body, method.generator, method.async);
+                    let right = types.functionExpression(key /* 函数的名字 这里传null就是匿名函数 */, method.params, method.body, method.generator, method.async);
 
                     let func = types.assignmentExpression('=', left, right)
                     body.push(func)
