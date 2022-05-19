084e639157519d4459cbcbed3a0c9885a5caa145
okbug <1583978087@qq.com>
2022-05-19 10:06:31 +0800

    1

--- records/repeat.js
+++ records/repeat.js
@@ -2,7 +2,7 @@ function repeat(str, n) {
   let result = ''
   if (n > 0) {
     while(true) {
-      if (n & 1) retult += str
+      if (n & 1) result += str
       n >>> 1
       if (n <= 0) break
       str += str
